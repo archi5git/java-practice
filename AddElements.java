5a8b71e85b71e4868593be31d9ceb1a57da9a1c9
archi5git <archimishra912019@gmail.com>
2026-06-27 12:48:29 +0530

    first

---
+++ AddElements.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class AddElements {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n+1];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int pos=sc.nextInt();
+        int element=sc.nextInt();
+        for(int i=n-1;i>=pos;i--){
+            arr[i]=arr[i-1];
+        }
+        arr[pos]=element;
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]);
+        }
+    }
+
+    
+}
