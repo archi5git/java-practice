ecf31f53307eebc878c5832c20e870b385272cb3
Archi Mishra <archimishra912019@gmail.com>
2026-09-13 14:57:01 +0530

    Time: 2 ms (60.74%), Space: 46.3 MB (81.76%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        // Use sets to track seen numbers for rows, columns, and boxes
+        HashSet<Character>[] rows = new HashSet[9];
+        HashSet<Character>[] cols = new HashSet[9];
+        HashSet<Character>[] boxes = new HashSet[9];
+        
+        for (int i = 0; i < 9; i++) {
+            rows[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+            boxes[i] = new HashSet<>();
+        }
+        
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char num = board[i][j];
+                if (num == '.') continue; // skip empty cells
+                
+                int boxIndex = (i / 3) * 3 + (j / 3);
+                
+                if (rows[i].contains(num) || cols[j].contains(num) || boxes[boxIndex].contains(num)) {
+                    return false; // duplicate found
+                }
+                
+                rows[i].add(num);
+                cols[j].add(num);
+                boxes[boxIndex].add(num);
+            }
+        }
+        
+        return true; // no conflicts found
+    }
+}
